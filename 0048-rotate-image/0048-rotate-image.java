02eb520b1722b58442026fe7441c92d853db60b9
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2024-06-29 15:23:10 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.11%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n= matrix.length;
+
+        //transpose
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+
+                //swap
+                int temp =matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
+            }
+        }
+
+        //reverse the transposed matrix
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+                
+
+            }
+
+        }
+        
+    }
+}
