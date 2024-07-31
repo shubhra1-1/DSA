955c2baa3af2d9d9dbae5f2793f4b7e33b04e9b3
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2024-07-31 10:08:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (49%) - LeetHub

---
+++ 1252-break-a-palindrome/1252-break-a-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        char [] strArr= palindrome.toCharArray();
+        if(strArr.length < 2){
+            return "";
+        }
+        for(int i=0;i<strArr.length/2;i++){
+            if(strArr[i]!='a'){
+                strArr[i]='a';
+                return String.valueOf(strArr);
+            }   
+        }
+        
+        strArr[strArr.length-1]='b';
+        return String.valueOf(strArr);
+    }
+}
