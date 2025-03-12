5692d2a044562f626e8810c03c7601a93f683084
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2025-03-12 20:13:16 +0530

    Time: 39 ms (40.12%), Space: 45.2 MB (29.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         Map<Integer, Integer> numToIndex = new HashMap<>();
-        
-        // Iterate through the list of numbers
-        for (int index = 0; index < nums.length; index++) {
-            int num = nums[index];
-            // Calculate the complement
-            int complement = target - num;
-            
-            // Check if the complement is already in the HashMap
-            if (numToIndex.containsKey(complement)) {
-                // Return the indices of the two numbers that add up to the target
-                return new int[] { numToIndex.get(complement), index };
+        int n= nums.length;
+        for(int i =0;i<n-1;i++){
+            for(int j =i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
             }
-            
-            // Add the current number and its index to the HashMap
-            numToIndex.put(num, index);
+
         }
-        
-        // If no solution is found, return an empty array (or throw an exception based on requirements)
-        return new int[] {};
+        return new int[]{}; //if no solution found
     }
-    
 }
