084917a7e8158e98ece3121140a824ba68920a1a
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2025-03-21 21:37:17 +0530

    Time: 2 ms (84.87%), Space: 45.8 MB (80.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int j=0;
-
         for(int i =0;i<nums.length;i++){
             if(nums[i]!=0){
                 int temp = nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
+                
                 j++;
             }
         }
-        
+  
     }
 }
