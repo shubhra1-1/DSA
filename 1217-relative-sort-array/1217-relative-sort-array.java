a6ef2aa9891f5c7b37f9a2d188b86d0aec753914
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2025-04-08 17:48:48 +0530

    Time: 2 ms (68.48%), Space: 42.5 MB (20.67%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-
-        Map<Integer, Integer> mp = new HashMap<>();
-
+        Map<Integer,Integer> mp = new HashMap<>();
         for(int arr : arr1){
-            mp.put(arr, mp.getOrDefault(arr,0)+1);
+            mp.put(arr,mp.getOrDefault(arr,0)+1);
         }
-
         int index=0;
-
         for(int arr:arr2){
             while(mp.getOrDefault(arr,0)>0){
-                arr1[index]=arr;
+                arr1[index] = arr;
                 index++;
                 mp.put(arr,mp.get(arr)-1);
-
 
             }
             mp.remove(arr);
@@ -23,17 +18,17 @@ class Solution {
         //remaining elements
         List<Integer> res = new ArrayList<>();
 
-        for(Map.Entry<Integer,Integer> entry: mp.entrySet()){
+        for(Map.Entry<Integer,Integer> entry : mp.entrySet() ){
             while(entry.getValue()>0){
                 res.add(entry.getKey());
                 entry.setValue(entry.getValue()-1);
             }
         }
-
         Collections.sort(res);
 
         for(int arr:res){
-            arr1[index++]=arr;
+            arr1[index]=arr;
+            index++;
         }
         return arr1;
     }
