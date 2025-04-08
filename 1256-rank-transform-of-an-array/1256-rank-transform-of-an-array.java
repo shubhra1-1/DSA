1c28b334ddb3b5e3e52cb2e44e5a5f97d1f5929e
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2025-04-08 13:57:47 +0530

    Time: 43 ms (26.47%), Space: 67.4 MB (5.13%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        Map<Integer, Integer> mp = new HashMap<>();
         Set<Integer> set = new TreeSet<>();
+        Map <Integer,Integer> mp = new HashMap<>();
 
-        //put elements of array in treeset
-        for(int num : arr ){
-            set.add(num);
+        for(int nums :arr){
+            set.add( nums);
+
         }
-        int rank=1; //bcoz rank start from 1
-        //put set elements in map
-        for(int maps: set){
+        int rank=1;
+        for(int maps : set){
             mp.put(maps,rank);
             rank++;
 
         }
+
         for(int i =0;i<arr.length;i++){
             arr[i]= mp.get(arr[i]);
         }
